f89c587428e3685ad6b6f5fec0342fc1e266e0f3
kszonek <k@kszonek.pl>
2022-07-04 19:26:59 +0000

    working ipv4 example to block dns

--- 31_network/network.bpf.c
+++ 31_network/network.bpf.c
@@ -5,6 +5,19 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
+#define DNS_PORT 53
+
+//#include <linux/pkt_cls.h>
+#define TC_ACT_OK		    0
+#define TC_ACT_RECLASSIFY	1
+#define TC_ACT_SHOT		    2
+#define TC_ACT_PIPE		    3
+#define TC_ACT_STOLEN		4
+#define TC_ACT_QUEUED		5
+#define TC_ACT_REPEAT		6
+#define TC_ACT_REDIRECT		7
+#define TC_ACT_TRAP		    8
+
 SEC("classifier")
 int handle_egress(struct __sk_buff *skb)
 {
@@ -12,46 +25,45 @@ int handle_egress(struct __sk_buff *skb)
     struct ethhdr *eth = (struct ethhdr*)(void*)(long)skb->data;
 
     if ((void*)eth + sizeof(*eth) + sizeof(struct iphdr) > data_end)
-        return 0;
+        return TC_ACT_OK;       // not IP
 
     if (eth->h_proto != bpf_htons(0x0800))
-        return 0;       // not IPv4
+        return TC_ACT_OK;       // not IPv4
 
-    bpf_printk("is ipv4");
-
+    // IP header
     struct iphdr *iph = (struct iphdr*)((void*)eth + sizeof(*eth));
-    struct tcphdr *tcph = NULL;
-    u8 hdr_sz = sizeof(struct iphdr);
 
+    // TCP header
+    struct tcphdr *tcph = (struct tcphdr*)((void*)iph + sizeof(struct iphdr));
+
+    // destination address
     uint32_t dest_addr = bpf_ntohl(iph->daddr);
 
-    // if (dest_addr == 0x1f3c9950)
+    // TCP/UDP port
+    int port = bpf_ntohs(BPF_CORE_READ(tcph, dest));
+
+    // if (dest_addr == 0x1f3de1fe)
     //     return 0;  // workaround: filter my SSH traffic
 
-    bpf_printk("dest ip is %x", dest_addr);
-
-    // struct udphdr *udph = NULL;
-    // if ((void*)(iph + hdr_sz + sizeof(*udph)) > data_end) {
-    //     bpf_printk("no udp");
-    //     return NULL;
+    // switch(iph->protocol)
+    // {
+    //     case IPPROTO_TCP:
+    //         bpf_printk("TCP %x:%d", dest_addr, port);
+    //         break;
+    //     case IPPROTO_UDP:
+    //         bpf_printk("UDP %x:%d", dest_addr, port);
+    //         break;
+    //     case IPPROTO_ICMP:
+    //         bpf_printk("ICMP %x:%d", dest_addr);
+    //     default:
+    //         return TC_ACT_OK;
     // }
 
-    // if ((void*)(iph + hdr_sz + sizeof(*tcph)) > data_end) {
-    //     bpf_printk("no tcp");
-    //     return 0;
-    // }
-
-    struct iphdr *v4 = (struct iphdr*)iph;
-    if (v4->protocol != IPPROTO_TCP)
+    if(iph->protocol == IPPROTO_UDP && port == DNS_PORT)
     {
-        bpf_printk("not tcp");
-        return 0;
+        bpf_printk("DNS blocked! %x:%d", dest_addr, port);
+        return TC_ACT_SHOT;
     }
-    bpf_printk("tcp");
-    tcph = (struct tcphdr*)((void*)iph + hdr_sz);
 
-
-
-    // bpf_printk("eth %x", BPF_CORE_READ(eth, h_proto));
-    return 0;
+    return TC_ACT_OK;
 }
